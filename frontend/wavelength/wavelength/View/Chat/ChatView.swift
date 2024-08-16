4c147e734fe719d429c2bd355fe718ce2efcf157
Doeun Kwon <dekwooon@gmail.com>
2024-08-15 21:59:14 -0700

    modify mock chat

--- frontend/wavelength/wavelength/View/Chat/ChatView.swift
+++ frontend/wavelength/wavelength/View/Chat/ChatView.swift
@@ -73,8 +73,6 @@ extension ChatMessage {
         ChatMessage(id: UUID().uuidString, content: "Hey Doeun! How can I help you?", dateCreated: Date(), sender: .ai),
         ChatMessage(id: UUID().uuidString, content: "What's the meaning of the universe?", dateCreated: Date(), sender: .user),
         ChatMessage(id: UUID().uuidString, content: "Deep question!  Universe = everything, all space & time. Science studies how it works, but meaning is more philosophical. Maybe it's out there, maybe it's for us to decide.", dateCreated: Date(), sender: .ai),
-        ChatMessage(id: UUID().uuidString, content: "What should I get Andrea for her birthday?", dateCreated: Date(), sender: .user),
-        ChatMessage(id: UUID().uuidString, content: "Andrea likes crafts, swimming & volleyball! For crafts, maybe a gift certificate to a store or a new kit like jewelry making or painting. For swimming, a new swimsuit, beach towel, goggles or fun floats could be cool! Volleyball ideas are a new ball, kneepads, or a volleyball t-shirt. Anything else you'd like to know about her interests? Might help narrow down the gift! ", dateCreated: Date(), sender: .ai),
         ChatMessage(id: UUID().uuidString, content: "Thanks!", dateCreated: Date(), sender: .user),
         ChatMessage(id: UUID().uuidString, content: "Always.", dateCreated: Date(), sender: .ai)]
 }
